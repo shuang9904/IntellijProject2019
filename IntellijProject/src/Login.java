9f8af6765ba3e118c6e97c15118e9f26cc6a2402
Lee Shang Huang <shuang9904@gmail.com>
2019-11-20 12:56:27 +0000

    Update 1

--- IntellijProject/src/Login.java
+++ IntellijProject/src/Login.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Login {
     public static void main(String[] args) {
-        String user="";
+        String userName;
+        int option;
 
-        user = JOptionPane.showInputDialog("Please enter your name");
+        //This will ask user to input the username
+        userName = JOptionPane.showInputDialog(null,"Please enter your name","Welcome", JOptionPane.INFORMATION_MESSAGE);
+
+        //Display option
+        option =JOptionPane.showOptionDialog(null, "Welcome " + userName + "\n\nWould you like to have a tutorial about this game?",
+                "Welcome", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+
+
+        //Ok to continue to the tutorial
+        if(option == JOptionPane.OK_OPTION)
+        {
+        }
+
+        else //If select cancel will proceed to the Main menu
+        {
+
+        }
+
+
 
 
     }
--- IntellijProject/src/MainMenu.java
+++ IntellijProject/src/MainMenu.java
@@ -9,37 +9,37 @@ public class MainMenu {
     private JButton exitButton;
     private JPanel MainMenu;
     private JButton startButton;
+    private JButton historyButton;
 
     public MainMenu() {
         exitButton.addActionListener(new ActionListener() {
-
 
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 int exitButton = JOptionPane.YES_NO_OPTION;
 
-                exitButton = JOptionPane.showConfirmDialog(null,"Are you sure you want to exit?","Warning", JOptionPane.YES_NO_OPTION);
-                if(exitButton == JOptionPane.YES_OPTION)
-                {
+                exitButton = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit?", "Warning", JOptionPane.YES_NO_OPTION);
+                if (exitButton == JOptionPane.YES_OPTION) {
                     System.exit(0);
-                }
-                else
-                {
+                } else {
 
                 }
 
             }
         });
     }
+
     //Main Menu GUI setup
     public static void main(String[] args) {
         JFrame frame = new JFrame("Main Menu");
         frame.setContentPane(new MainMenu().MainMenu);
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(500,500));
+        frame.setMinimumSize(new Dimension(500, 500));
         frame.pack();
         frame.setVisible(true);
     }
+
+
 
 }
